7c64f28e25eec134d067aae3fbe1a55f37c75ffa
cheikh faye <cheikhfaye150193d@gmail.com>
2023-01-18 23:06:18 +0000

    d

--- routes/post.js
+++ routes/post.js
@@ -43,8 +43,8 @@ db.query(q,[],(err, data) => {
     })
     router.get("/", (req,res) => {
         const q =  req.query.categorie ?
-        "SELECT DISTINCT id,title,content,categorie,date,name,username,email FROM post p INNER JOIN user u ON p.uid = u.id where categorie = ?" :
-        "SELECT DISTINCT id,title,content,categorie,date,name,username,email FROM post p INNER JOIN user u ON p.uid = u.id"
+        "SELECT DISTINCT id,title,content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid where categorie = ?" :
+        "SELECT DISTINCT id,title,content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid"
         db.query(q,[
             req.query.categorie
         ],(err, data) => {
