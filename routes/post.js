af592d246ecd10ad8d33d3328193aa8922885605
cheikh faye <cheikhfaye150193d@gmail.com>
2023-01-24 15:51:16 +0000

    deploy n

--- routes/post.js
+++ routes/post.js
@@ -43,8 +43,8 @@ db.query(q,[],(err, data) => {
     })
     router.get("/", (req,res) => {
         const q =  req.query.categorie ?
-        "SELECT DISTINCT id_post,title,content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid where categorie = ?" :
-        "SELECT DISTINCT id_post,title,content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid"
+        "SELECT DISTINCT id_post,title, likes, content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid where categorie = ?" :
+        "SELECT DISTINCT id_post,title, likes, content,categorie,date,name,username,email FROM user INNER JOIN post ON user.id = post.uid"
         db.query(q,[
             req.query.categorie
         ],(err, data) => {
@@ -54,7 +54,7 @@ db.query(q,[],(err, data) => {
         })  
     })
     router.get("/:id",(req, res) => {
-            const q = "SELECT title, content, categorie,email FROM post p INNER JOIN user u ON P.uid = u.id where id=?"
+            const q = "SELECT title, likes,content, categorie,email FROM post p INNER JOIN user u ON p.uid = u.id where id_post=?"
     db.query(q,[req.params.id],(err, data) => {
         if(err) return res.status(501).json(err)
         res.status(201).json(data)
@@ -66,5 +66,26 @@ db.query(q,[],(err, data) => {
         if(err) return res.status(501).json(err)
         res.status(201).json(data)
     })
+    })
+
+    // update Post 
+    router.put("/:id", (req, res) => {
+        const q = "UPDATE post SET `title`= ?,`content` =?, `categorie`=? WHERE `id_post`=? "
+        const values = [req.body.title, req.body.content, req.body.categorie]
+        db.query(q,[...values, req.params.id],(err, data) => {
+            if(err) return res.status(500).json(err)
+           return res.status(201).json("post updated")
+        })
+    }) 
+
+    // add Like
+    router.put("/like/:id", (req, res) => {
+        const q = "UPDATE post SET `likes`=? WHERE `id_post`=?"
+        const values = [req.body.likes]
+
+        db.query(q,[...values, req.params.id], (err, data= dataInfo ) => {
+            if(err) return res.status(502).json(err)
+            return res.status(201).json(data)
+        })
     })
     module.exports = router
