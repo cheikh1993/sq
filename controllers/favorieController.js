d1bfd4c9ad023e896d523f0ed0fdd1f582971ed9
cheikh faye <cheikhfaye150193d@gmail.com>
2023-03-02 20:47:49 +0000

    mod

--- controllers/favorieController.js
+++ controllers/favorieController.js
@@ -5,10 +5,8 @@ const db = require("../db")
 const getFavoriePost = (req, res) => {
   const q = "SELECT id_post,title,content,categorie,likes,img,date,name,username,email FROM favorie INNER JOIN post ON favorie.id=post.id_post INNER JOIN user ON favorie.id=user.id WHERE favorie.uid=?"
   try {
-    const values = [
-      req.query.userId
-    ]
-    db.query(q, [values], (err, data) => {
+   
+    db.query(q, [req.query.uid], (err, data) => {
       if (err) return res.status(500).json(err)
       if (data.length === 0) return res.status(401).json("Vos favories sont vides")
       res.status(200).json(data)
